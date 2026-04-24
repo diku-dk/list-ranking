685b3ff450db25b7d96c73d0de8d24c077a535a0
Due <williamhenrichdue@gmail.com>
2026-04-24 13:41:47 +0200

    Correct this.

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH --gres=gpu:a100
-#SBATCH --mem=16G
-#SBATCH --exclude=hendrixgpu05fl
+#SBATCH --mem=32G
+#SBATCH --exclusive
 #SBATCH --time=0:30:00
 
 module unload cuda
 module load cuda/11.8
-futhark bench --backend=cuda list-ranking.fut
+futhark bench --backend=cuda list_ranking.fut
